a9284882ade82ee2bcaacd892c626b1168722d5c
hajarfawzi <76879784+hajarfawzi@users.noreply.github.com>
2021-01-18 21:03:54 +0200

    Update CourseCatalogAction.cpp

--- CourseCatalogAction.cpp
+++ CourseCatalogAction.cpp
@@ -11,3 +11,35 @@
 ///*ofstream Course_Catalog;
 //
 //	Course_Catalog.open("D:\\ZC\\C++\\Project\\Course_Catalog");*/
+
+
+
+/*
+fstream file;
+	//assume that course catalog file is CourseCatalog.txt
+	ifstream finput("CourseCatalog.txt");
+	CourseInfo coinfo;
+	string corequesit;
+	string prerequesit;
+	string data;
+	string co;
+	string core;
+	while (getline(file, data))
+	{
+		stringstream  s(data);
+		getline(s, coinfo.Code, ',');
+		getline(s, coinfo.Title, ',');
+		getline(s, co, ',');
+		coinfo.Credits = stoi(co);
+		getline(s, core, ',');
+		while (coinfo.CoReqList.empty() != NULL)
+		{
+			coinfo.CoReqList.push_back(corequesit);
+		}
+		getline(s, prerequesit, ',');
+		while (coinfo.PreReqList.empty() != NULL)
+		{
+			coinfo.PreReqList.push_back(corequesit);
+		}
+		RegRules.CourseCatalog.push_back(coinfo);
+	}*/
