d1e0917c9ed85b8f54b3d1d8a970d7a301a86ba6
YasminaPPP <77639846+YasminaPPP@users.noreply.github.com>
2021-01-18 22:26:48 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -17,6 +17,7 @@ class Course : public Drawable
 	bool Done;		//Done or not yet?
 	list<Course_Code> PreReq;	//list of prerequisites
 	list<Course_Code> CoReq;	//list of prerequisites
+	bool show;
 public:
 	Course(Course_Code r_code,string r_title, int crd);
 	string getTitle() const;
