9544be6cc4222fdec605e10f30ca08ca82d0d3ff
riham-a <75180334+riham-a@users.noreply.github.com>
2021-01-18 20:07:39 +0200

    Add files via upload

---
+++ Course.cpp
@@ -0,0 +1,35 @@
+#include "Course.h"
+#include "../GUI/GUI.h"
+#include <fstream>
+#include <sstream>
+using namespace std;
+Course::Course(Course_Code r_code, string r_title, int crd):code(r_code),Title(r_title)
+{
+	credits = crd;
+}
+
+Course::~Course()
+{
+}
+
+Course_Code Course::getCode() const
+{
+	return code;
+}
+
+string Course::getTitle() const
+{
+	return Title;
+}
+
+//return course credits
+int Course::getCredits() const
+{
+	return credits;
+}
+
+void Course::DrawMe(GUI* pG) const
+{
+	pG->DrawCourse(this);
+	
+}
---
+++ Course.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+#include <list>
+using namespace std;
+#include "..\DEFs.h"
+#include <fstream>
+#include <iostream>
+#include "../GUI/Drawable.h"
+
+//Base class for all types of courses
+class Course : public Drawable
+{
+	const Course_Code code;	//course code: e.g. "CIE202". This is the course ID
+	const string Title;		//course title: e.g. "Fundamentals of Computer Programming"
+	int credits;	//no. of course credits
+	string type;	//Univ, track, or major.....etc.
+	bool Done;		//Done or not yet?
+	list<Course_Code> PreReq;	//list of prerequisites
+	list<Course_Code> CoReq;	//list of prerequisites
+public:
+	Course(Course_Code r_code,string r_title, int crd);
+	string getTitle() const;
+	string getCode() const;
+	int getCredits() const;
+	Course_Code getPreReq() const;
+	Course_Code getcoReq() const;
+	void DrawMe(GUI*) const;
+	char* pch;
+	char* context = nullptr;
+	char line[300]; 
+	char* point = line;
+	void importCourseCatalog() const;
+	virtual ~Course();
+
+};
