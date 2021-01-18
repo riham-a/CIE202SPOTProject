429602f1eb19f6122e56da4da55e4ce7575f4f0f
YasminaPPP <77639846+YasminaPPP@users.noreply.github.com>
2021-01-18 22:28:33 +0200

    Update Course.cpp

--- Course.cpp
+++ Course.cpp
@@ -3,9 +3,13 @@
 #include <fstream>
 #include <sstream>
 using namespace std;
+Course::Course(){
+	show=true;
+}
 Course::Course(Course_Code r_code, string r_title, int crd):code(r_code),Title(r_title)
 {
 	credits = crd;
+	show=true;
 }
 
 Course::~Course()
