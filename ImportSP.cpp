ebe44948599d38bc05304ad689a562079e59113c
Esraa-M27 <75180500+Esraa-M27@users.noreply.github.com>
2021-01-19 04:28:27 +0200

    Update ImportSP.cpp

--- ImportSP.cpp
+++ ImportSP.cpp
@@ -1,33 +1,47 @@
 #include <vector>
+#include "../GUI/Drawable.h"
+#include "StudyPlan.h"
+#include "Action.h"
 #include "ImportSP.h"
-#include <fstream> 
-#include "StudyPlan/StudyPlan.h"
-StudyPlan* ImportSP::getStudyPlay() const
+#include <ifstream> 
+#include "Registrar.h"
+using namespace std;
+
+StudyPlan*  ImportSP::savedPlan() const 
 {
+	pReg-> getStudyPlan()->pSPlan;
 	return pSPlan;
+
+	GUI* pGUI = pReg->getGUI();
+
 }
 bool ImportSP::Execute() {
 	GUI* pGUI = pReg->getGUI();
 
-	ifstream StudyPlan;
-	StudyPlan.open("D:\\UNI\\CIE \\ C++ \\ project\\DeafultSP");
-	vector <Course_Code> StudyPlan;
-	 double size = 250;
-	char line[250];
-	while (!StudyPlan.getline(line, size) 
-	{
-		pch = strtok_s(line, ",", &context);
+	ifstream studyPlan;
+	studyPlan.open("D:\\UNI\\CIE \\ C++ \\ project\\DeafultSP");
+	vector <Course_Code> TheStudyPlan;
+	double size = 250;
+	char line[size];
+	char* pch;
+	char* Context;
+	StudyPlan* pCourses;
+
+	
+	while (!StudyPlan.getline(line, size) {
+		pch = strtok_s(line, ",", &Context);
 		while (pch != NULL)
 		{
 
 			pch = strtok_s(NULL, ",", &context);
-		}
-		StudyPlan >> Course_Code theplan;
-		Course_Code.push_back(theplan);
+			TheStudyPlan.push_back(Context);
+        }
+		
 	}
-	for (int i; i < StudyPlan.size(); i++) {
-		if (default == StudyPlan[i])
-			plan.push_back(default);
+	for (int i; i < TheStudyPlan.size(); i++) {
+		pCourses->pReg;
 	}
+	GUI* pGUI = pReg->getGUI();
+
 	return true;
 }
