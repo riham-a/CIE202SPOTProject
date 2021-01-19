48bf40a59424d39132de27f9d49324d0e04a945f
riham-a <75180334+riham-a@users.noreply.github.com>
2021-01-19 23:08:55 +0200

    Update ValidityReportAction.cpp

--- ValidityReportAction.cpp
+++ ValidityReportAction.cpp
@@ -8,11 +8,7 @@ ValidityReportAction::ValidityReportAction(Registrar* VR) :Action(VR)
 }
 bool ValidityReportAction::Execute()
 {
-	GUI* pGUI = pReg->getGUI();
-
-	pGUI->PrintMsg("Choose your Second Major");
-	string Second_Major = pGUI->GetSrting();
-	StudyPlan* S = pReg->getStudyPlay();
-	S->setDoubleMajor(true);
+	StudyPlan* SP;
+	SP->getReport();
 	return true;
 }
